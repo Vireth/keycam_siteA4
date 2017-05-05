6fa02b7b4cc2c384fb2bb9b74bfae7f9bd0c3416
Inferno <aymeric.gand@epitech.eu>
2017-05-05 17:42:07 +0800

    Cadre Team
    Creation des cadres des membre de team

--- src/app/team.component.ts
+++ src/app/team.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team',
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent {
-
+  public name = 'Simon';
+  public student_id = '123456789';
+  public role = 'Dev';
+  public img = '';
   constructor(){}
 }
