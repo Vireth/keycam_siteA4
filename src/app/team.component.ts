0f83a7504770f655b47ab7f9c2ebdfa4522e90bb
Inferno <aymeric.gand@epitech.eu>
2017-05-08 01:48:00 +0800

    Page Team ok
    NgFor OK
    HTML OK
    CSS OK

--- src/app/team.component.ts
+++ src/app/team.component.ts
@@ -3,12 +3,82 @@
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
+  template: `
+  <h1 style="text-align: center">Our team group</h1>
+  <br>
+  <div class="row center" *ngFor="let group of data">
+    <div *ngFor="let person of group.person" class="card">
+      <div class="header-bg">
+        <h3>{{person.name}}</h3>
+      </div>
+      <div class="avatar">
+        <img src="{{'../assets/team/' + person.img}}" alt="keysim" />
+      </div>
+      <p class="card-desc">{{person.student_id}}<br>{{person.role}}</p>
+    </div>
+  </div>
+ `    
 })
 export class TeamComponent {
-  public name = 'Simon';
-  public student_id = '123456789';
-  public role = 'Dev';
-  public img = '';
+  data: any[] = [
+    {
+      'team': 'web',
+      'person': [
+        {
+          "name": "Victoria Hjalmarsson",
+          "img": "victoria.png",
+          "student_id": "16920035",
+          "role": "Project manager"
+        },
+        {
+          "name": "Simon Menard",
+          "img": "keysim.png",
+          "student_id": "16129161",
+          "role": "Dev Web Site and API"
+        },
+      ]
+    },
+    {
+      'team': 'android',
+      'person': [
+        {
+          "name": "Maxime Lamarthe",
+          "img": "maxime.png",
+          "student_id": "16129095",
+          "role": "Dev Web Site"
+        },
+        {
+          "name": "Jean-Luc Tang",
+          "img": "tang.png",
+          "student_id": "16129131",
+          "role": "Dev Android"
+        },
+        {
+          "name": "Vireth Thach sok",
+          "img": "vireth.png",
+          "student_id": "16129114",
+          "role": "Dev Android"
+        }
+      ]
+    },
+    {
+      'team': 'video',
+      'person': [
+        {
+          "name": "Youh Iddahamou",
+          "img": "youh.png",
+          "student_id": "16129157",
+          "role": "Dev streaming video"
+        },
+        {
+          "name": "Aymeric Gand",
+          "img": "aymeric.png",
+          "student_id": "16129137",
+          "role": "Dev streaming video"
+        }
+      ]
+    }
+  ];
   constructor(){}
 }
