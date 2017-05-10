46fd86b7e15750e661455590b95c0f3848f4ce85
Inferno <aymeric.gand@epitech.eu>
2017-05-10 16:36:03 +0800

    Team Page
    Team Page in Pokeball and responsive

--- src/app/team.component.ts
+++ src/app/team.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component } from '@angular/core';
-import {KeycamService} from './Service/keycam.service';
+import { Headers, Http } from '@angular/http';
+
 import 'rxjs/add/operator/toPromise';
 
 @Component({
@@ -8,10 +9,65 @@ import 'rxjs/add/operator/toPromise';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent {
-  other;
+  private url = 'http://10.0.1.6:3333/api/teams';
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  data: any[] = [
+    {
+      'person': [
+        {
+          'name': 'Victoria Hjalmarsson',
+          'img': 'victoria.png',
+          'student_id': '16920035',
+          'role': 'Project manager'
+        },
+        {
+          'name': 'Simon Menard',
+          'img': 'keysim.png',
+          'student_id': '16129161',
+          'role': 'Dev Web Site and API'
+        },
+      ]
+    },
+    {
+      'person': [
+        {
+          'name': 'Maxime Lamarthe',
+          'img': 'maxime.png',
+          'student_id': '16129095',
+          'role': 'Dev Web Site'
+        },
+        {
+          'name': 'Jean-Luc Tang',
+          'img': 'tang.png',
+          'student_id': '16129131',
+          'role': 'Dev Android'
+        },
+        {
+          'name': 'Vireth Thach sok',
+          'img': 'vireth.png',
+          'student_id': '16129114',
+          'role': 'Dev Android'
+        }
+      ]
+    },
+    {
+      'person': [
+        {
+          'name': 'Youh Iddahamou',
+          'img': 'youh.png',
+          'student_id': '16129157',
+          'role': 'Dev streaming video'
+        },
+        {
+          'name': 'Aymeric Gand',
+          'img': 'aymeric.png',
+          'student_id': '16129137',
+          'role': 'Dev streaming video'
+        }
+      ]
+    }
+  ];
 
-  constructor(private keycamService: KeycamService) {
-    this.keycamService.team()
-      .then(response => this.other = response);
+  constructor(private http: Http) {
   }
 }
