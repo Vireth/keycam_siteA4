72f119530aa0e432baaf3da50ca13567270667c5
vireth20 <mdk2-kevin@hotmail.fr>
2017-05-10 16:28:33 +0800

    fix

--- src/app/team.component.ts
+++ src/app/team.component.ts
@@ -1,8 +1,6 @@
 ﻿import { Component } from '@angular/core';
-import { Headers, Http } from '@angular/http';
 import {KeycamService} from './Service/keycam.service';
 import 'rxjs/add/operator/toPromise';
-import {parseHttpResponse} from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-team',
@@ -10,16 +8,6 @@ import {parseHttpResponse} from 'selenium-webdriver/http';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent {
-  // private url = 'http://10.0.1.6:3333/api/teams';
-  // private headers = new Headers({ 'Content-Type': 'application/json' });
-  // other;
-  //
-  // constructor(private keycamService: KeycamService) {
-  //   this.http.get(this.url, { headers: this.headers })
-  //     .toPromise()
-  //     .then(data => this.other = data.json());
-  // }
-
   other;
 
   constructor(private keycamService: KeycamService) {
